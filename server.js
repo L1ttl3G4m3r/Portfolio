cf9d8006102165cc027340b94fc7484e4d2de517
Kaylee Hoek <kayleehoek04.17@gmail.com>
2025-04-10 18:20:26 +0200

    begin localhost

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world! This is your Express server.');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
